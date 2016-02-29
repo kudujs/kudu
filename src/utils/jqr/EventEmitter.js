2d6a993a32ec28139d154a2d66c684cc6223aba2
Bob Schellink <sabob1@gmail.com>
2016-02-29 20:28:22 +0200

    wrong number of arguments sliced

--- src/utils/jqr/EventEmitter.js
+++ src/utils/jqr/EventEmitter.js
@@ -408,7 +408,7 @@
      * @return {Object} Current instance of EventEmitter for chaining.
      */
     proto.emit = function emit(that, evt) {
-        var args = Array.prototype.slice.call(arguments, 1);
+        var args = Array.prototype.slice.call(arguments, 2);
         return this.emitEvent(that, evt, args);
     };
 
